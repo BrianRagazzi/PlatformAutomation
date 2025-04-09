06a5accb40ebd30f6b246b75e524786ea6a37b84
Brian Ragazzi <brianpragazzi@gmail.com>
2025-04-09 14:46:11 -0500

    revert some changes

--- tasks/functions/nsxt4.sh
+++ tasks/functions/nsxt4.sh
@@ -111,7 +111,7 @@ Create_NSX_T1_Gateway() {
    echo "Creating $1"
    local t0path=$(Get_NSX_T0_Gateway_Path $2)
    #local routeadv='["TIER1_NAT","TIER1_LB_VIP","TIER1_CONNECTED","TIER1_IPSEC_LOCAL_ENDPOINT"]'
-   gateway_config=$(jq -c \
+   gateway_config=$(jq -n \
      --arg display_name "$1" \
      --arg t0path "$t0path" \
      --argjson route_advertisement_types $routeadv \
@@ -132,9 +132,9 @@ Create_NSX_T1_Gateway() {
      '
      )
     # Create Gateway
-    response=$(curl -s -k -H "Content-Type: Application/json" -H "X-Allow-Overwrite: true" \
+    curl -s -k -H "Content-Type: Application/json" -H "X-Allow-Overwrite: true" \
      -u $NSXUSERNAME:$NSXPASSWORD \
-     $NSXHOSTNAME/policy/api/v1/infra/tier-1s/$1 -X PATCH -d "$gateway_config")
+     $NSXHOSTNAME/policy/api/v1/infra/tier-1s/$1 -X PATCH -d "$gateway_config"
 
      # Attach to edge cluster
     # https://developer.broadcom.com/xapis/nsx-t-data-center-rest-api/latest/method_PatchTier1LocaleServices.html
